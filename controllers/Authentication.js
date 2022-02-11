51d2a2efd4ec2596f82fba94f22ba98d9e9cee99
To The Nam <tothenam@Tos-MacBook-Pro.local>
2022-02-11 09:37:25 +0700

    Update v2

--- controllers/Authentication.js
+++ controllers/Authentication.js
@@ -1,66 +1,67 @@
 import User from "../models/User.js";
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { v1 } from "uuid";
-import { registerSchema } from "../validation/register.js";
+import { registerSchema, autheticationSchema } from "../validation/register.js";
 
 const uuidv1 = v1;
 
-export const register = async (req, res) => {  
+export const register = async (req, res) => {
   try {
-    await registerSchema.validateAsync(req.body, {abortEarly: false})
-    const { email, password, firstName, lastName} = req.body;
-    const findUser = await User.findOne({ email});
-    if(findUser){
+    await registerSchema.validateAsync(req.body, { abortEarly: false });
+    const { email, password, firstName, lastName } = req.body;
+    const findUser = await User.findOne({ email });
+    if (findUser) {
       res.status(200).json({
-        message:`Email ${email} already available!`
+        message: `Email ${email} already available!`,
       });
     } else {
       const user = new User({
         email,
-        password, 
-        firstName, 
+        password,
+        firstName,
         lastName,
-        image: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        image: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
       });
       await user.save();
-      const findUserToLogin = await User.find({email});
+      const findUserToLogin = await User.find({ email });
       const secretKey = uuidv1();
-      const token = jwt.sign({_id: findUserToLogin._id}, `${secretKey}`);
+      const token = jwt.sign({ _id: findUserToLogin._id }, `${secretKey}`);
       res.status(200).json({
         secretKey,
         token,
-        message:`Email ${email} registesred successfully!`
+        message: `Email ${email} registesred successfully!`,
       });
-    };
+    }
   } catch (err) {
-    res.status(500).json({error: err});
+    res.status(500).json({ error: err });
   }
 };
 
 export const signIn = async (req, res) => {
   try {
-    const {email, password} = req.body;
-    const findUser = await User.findOne({ email});
-    if (findUser){
+    await autheticationSchema.validateAsync(req.body, { abortEarly: false });
+    const { email, password } = req.body;
+    const findUser = await User.findOne({ email });
+    if (findUser) {
       if (findUser.password === password) {
         const secretKey = uuidv1();
-        const token = jwt.sign({_id: findUser._id}, `${secretKey}`);
+        const token = jwt.sign({ _id: findUser._id }, `${secretKey}`);
         res.status(200).json({
           secretKey,
           token,
-          message:'Sign In successfully!'
+          message: "Sign In successfully!",
         });
       } else {
         res.status(200).json({
-          message: 'Invalid Password, Please try again'
+          message: "Invalid Password, Please try again",
         });
       }
     } else {
       res.status(200).json({
-        message: `Email ${email} not available!`
+        message: `Email ${email} not available!`,
       });
     }
   } catch (err) {
-    res.status(500).json({errror: err});
+    res.status(500).json({ errror: err });
   }
 };
--- validation/register.js
+++ validation/register.js
@@ -1,8 +1,13 @@
-import Joi from '@hapi/joi';
+import Joi from "@hapi/joi";
 
 export const registerSchema = Joi.object({
-  email: Joi.string().email().lowercase().trim().required(), 
-  password: Joi.string().min(3).max(16).required(), 
-  firstName: Joi.string().min(3).max(256).trim().required(),  
+  email: Joi.string().email().lowercase().trim().required(),
+  password: Joi.string().min(3).max(16).required(),
+  firstName: Joi.string().min(3).max(256).trim().required(),
   lastName: Joi.string().min(3).max(256).trim().required(),
+});
+
+export const autheticationSchema = Joi.object({
+  email: Joi.string().email().lowercase().trim().required(),
+  password: Joi.string().min(3).max(16).required(),
 });
